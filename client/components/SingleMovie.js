5b9d4db51fb09e4df11ec7ba9cc129f1fd5fb0b4
Dylan Horgan <dyhorgan@gmail.com>
2020-03-14 23:58:14 -0400

    added genre and service filters

--- client/components/SingleMovie.js
+++ client/components/SingleMovie.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+// import IMDB from './icons8-imdb-48.png'
 const dummyData = [
   {
     title: 'Good Will Hunting',
@@ -6,6 +7,8 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Good_Will_Hunting.png/220px-Good_Will_Hunting.png',
     imdbScore: 8.3,
     rottenTomatoes: 97,
+    year: 1997,
+    whereWatch: 'Hulu',
     id: 1
   },
   {
@@ -14,6 +17,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BZmY2NjUzNDQtNTgxNC00M2Q4LTljOWQtMjNjNDBjNWUxNmJlXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg',
     imdbScore: 8.5,
     rottenTomatoes: 98,
+    year: 1993,
+    whereWatch: 'Hulu',
     id: 2
   },
   {
@@ -22,6 +27,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjI0MDMzNTQ0M15BMl5BanBnXkFtZTgwMTM5NzM3NDM@._V1_.jpg',
     imdbScore: 7.5,
     rottenTomatoes: 95,
+    year: 2018,
+    whereWatch: 'Hulu',
     id: 3
   },
   {
@@ -30,6 +37,8 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/Warrior_Poster.jpg/220px-Warrior_Poster.jpg',
     imdbScore: 8.2,
     rottenTomatoes: 83,
+    year: 2011,
+    whereWatch: 'Hulu',
     id: 4
   },
   {
@@ -38,6 +47,8 @@ const dummyData = [
       'https://image.tmdb.org/t/p/original/3gzLGFBoJM1xlNk0Ulzp0cJ6uPp.jpg',
     imdbScore: 7.8,
     rottenTomatoes: 96,
+    year: 1993,
+    whereWatch: 'Hulu',
     id: 5
   },
   {
@@ -46,6 +57,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BZGQxOTFjNWQtMjQ0Yi00OWU1LWFlZmEtNGFhMmRlYjg3N2IzL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
     imdbScore: 7.6,
     rottenTomatoes: 86,
+    year: 2003,
+    whereWatch: 'Hulu',
     id: 6
   },
   {
@@ -54,6 +67,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM@._V1_UY1200_CR90,0,630,1200_AL_.jpg',
     imdbScore: 8.0,
     rottenTomatoes: 90,
+    year: 1988,
+    whereWatch: 'Hulu',
     id: 7
   },
   {
@@ -62,6 +77,8 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Big_Fish_movie_poster.png/220px-Big_Fish_movie_poster.png',
     imdbScore: 8.0,
     rottenTomatoes: 75,
+    year: 2003,
+    whereWatch: 'Hulu',
     id: 8
   },
   {
@@ -70,6 +87,8 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Hunt_for_the_Wilderpeople.png/220px-Hunt_for_the_Wilderpeople.png',
     imdbScore: 7.9,
     rottenTomatoes: 96,
+    year: 2016,
+    whereWatch: 'Hulu',
     id: 9
   },
   {
@@ -77,6 +96,8 @@ const dummyData = [
     imgUrl: 'https://upload.wikimedia.org/wikipedia/en/9/9c/Free_Solo.png',
     imdbScore: 8.2,
     rottenTomatoes: 97,
+    year: 2018,
+    whereWatch: 'Hulu',
     id: 10
   },
   {
@@ -85,6 +106,8 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/WhenHarryMetSallyPoster.jpg/220px-WhenHarryMetSallyPoster.jpg',
     imdbScore: 7.6,
     rottenTomatoes: 90,
+    year: 1989,
+    whereWatch: 'Hulu',
     id: 11
   },
   {
@@ -93,6 +116,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjI5MDY1NjYzMl5BMl5BanBnXkFtZTgwNjIzNDAxNDM@._V1_.jpg',
     imdbScore: 7.5,
     rottenTomatoes: 89,
+    year: 2017,
+    whereWatch: 'Hulu',
     id: 12
   },
   {
@@ -101,6 +126,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjE5OTM0OTY5NF5BMl5BanBnXkFtZTgwMDcxOTQ3ODE@._V1_.jpg',
     imdbScore: 7.9,
     rottenTomatoes: 82,
+    year: 2016,
+    whereWatch: 'Hulu',
     id: 13
   },
   {
@@ -109,6 +136,8 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BNTUxNzYyMjg2N15BMl5BanBnXkFtZTcwMTExNzExNw@@._V1_.jpg',
     imdbScore: 7.0,
     rottenTomatoes: 91,
+    year: 2011,
+    whereWatch: 'Hulu',
     id: 14
   },
   {
@@ -117,7 +146,20 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMTI3MzYxMTA4NF5BMl5BanBnXkFtZTcwMDE4ODg3Mg@@._V1_.jpg',
     imdbScore: 7.4,
     rottenTomatoes: 91,
+    year: 2009,
+    whereWatch: 'Hulu',
     id: 15
+  },
+  {
+    title: 'Booksmart',
+    imgUrl:
+      'https://m.media-amazon.com/images/M/MV5BMjEzMjcxNjA2Nl5BMl5BanBnXkFtZTgwMjAxMDM2NzM@._V1_.jpg',
+    imdbScore: 7.2,
+    rottenTomatoes: 97,
+    year: 2019,
+    tag: ['All', 'Comedy'],
+    whereWatch: 'Hulu',
+    id: 16
   }
 ]
 export class SingleMovie extends Component {
@@ -138,17 +180,34 @@ export class SingleMovie extends Component {
   render() {
     console.log('logging found', this.state.found)
     console.log('logging id', this.state.id)
+    const found = this.state.found
     return (
-      <div>
-        <h1>Single Movie Component</h1>
-        <p>{this.state.found.title}</p>
-        <img
-          src={this.state.found.imgUrl}
-          className="responsive-img"
-          alt="something"
-        />
-        <p>{this.state.found.imdbScore}</p>
-        <p>{this.state.found.rottenTomatoes}</p>
+      <div className="container center-align">
+        <h2>{found.title}</h2>
+        <h5>{found.year}</h5>
+
+        <img src={found.imgUrl} className="singleStyle" alt="something" />
+        <div className="center-align">
+          <p className="flexRow valign-wrapper center-align">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+              className="logoStyle"
+            />
+            <p id="styleSpacing" />
+            {found.imdbScore}
+          </p>
+          <p className="flexRow valign-wrapper center-align">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/1009px-Rotten_Tomatoes.svg.png"
+              className="logoStyle"
+            />
+            <p id="styleSpacing" />
+            <p>{found.rottenTomatoes}%</p>
+          </p>
+        </div>
+        <h4>
+          Where to Watch {found.title}: {found.whereWatch}
+        </h4>
       </div>
     )
   }
--- client/components/allMovies.js
+++ client/components/allMovies.js
@@ -18,13 +18,16 @@ import {Link} from 'react-router-dom'
 //  })
 // });
 
-const dummyData = [
+export let dummyDataOriginal = [
   {
     title: 'Good Will Hunting',
     imgUrl:
       'https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Good_Will_Hunting.png/220px-Good_Will_Hunting.png',
     imdbScore: 8.3,
     rottenTomatoes: 97,
+    year: 1997,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 1
   },
   {
@@ -33,6 +36,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BZmY2NjUzNDQtNTgxNC00M2Q4LTljOWQtMjNjNDBjNWUxNmJlXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg',
     imdbScore: 8.5,
     rottenTomatoes: 98,
+    year: 1993,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 2
   },
   {
@@ -41,6 +47,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjI0MDMzNTQ0M15BMl5BanBnXkFtZTgwMTM5NzM3NDM@._V1_.jpg',
     imdbScore: 7.5,
     rottenTomatoes: 95,
+    year: 2018,
+    genreTag: ['All', 'Horror'],
+    whereWatch: ['All', 'Hulu'],
     id: 3
   },
   {
@@ -49,6 +58,9 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/Warrior_Poster.jpg/220px-Warrior_Poster.jpg',
     imdbScore: 8.2,
     rottenTomatoes: 83,
+    year: 2011,
+    genreTag: ['Action/Adventure', 'All'],
+    whereWatch: ['All', 'Hulu'],
     id: 4
   },
   {
@@ -57,6 +69,9 @@ const dummyData = [
       'https://image.tmdb.org/t/p/original/3gzLGFBoJM1xlNk0Ulzp0cJ6uPp.jpg',
     imdbScore: 7.8,
     rottenTomatoes: 96,
+    year: 1993,
+    genreTag: ['Action/Adventure', 'All'],
+    whereWatch: ['All', 'Hulu'],
     id: 5
   },
   {
@@ -65,6 +80,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BZGQxOTFjNWQtMjQ0Yi00OWU1LWFlZmEtNGFhMmRlYjg3N2IzL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
     imdbScore: 7.6,
     rottenTomatoes: 86,
+    year: 2003,
+    genreTag: ['All', 'Horror'],
+    whereWatch: ['All', 'Hulu'],
     id: 6
   },
   {
@@ -73,6 +91,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM@._V1_UY1200_CR90,0,630,1200_AL_.jpg',
     imdbScore: 8.0,
     rottenTomatoes: 90,
+    year: 1988,
+    genreTag: ['Action/Adventure', 'All'],
+    whereWatch: ['All', 'Hulu'],
     id: 7
   },
   {
@@ -81,6 +102,9 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Big_Fish_movie_poster.png/220px-Big_Fish_movie_poster.png',
     imdbScore: 8.0,
     rottenTomatoes: 75,
+    year: 2003,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 8
   },
   {
@@ -89,6 +113,9 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Hunt_for_the_Wilderpeople.png/220px-Hunt_for_the_Wilderpeople.png',
     imdbScore: 7.9,
     rottenTomatoes: 96,
+    year: 2016,
+    genreTag: ['All', 'Comedy'],
+    whereWatch: ['All', 'Hulu'],
     id: 9
   },
   {
@@ -96,6 +123,9 @@ const dummyData = [
     imgUrl: 'https://upload.wikimedia.org/wikipedia/en/9/9c/Free_Solo.png',
     imdbScore: 8.2,
     rottenTomatoes: 97,
+    year: 2018,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 10
   },
   {
@@ -104,6 +134,9 @@ const dummyData = [
       'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/WhenHarryMetSallyPoster.jpg/220px-WhenHarryMetSallyPoster.jpg',
     imdbScore: 7.6,
     rottenTomatoes: 90,
+    year: 1989,
+    genreTag: ['All', 'Comedy'],
+    whereWatch: ['All', 'Hulu'],
     id: 11
   },
   {
@@ -112,6 +145,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjI5MDY1NjYzMl5BMl5BanBnXkFtZTgwNjIzNDAxNDM@._V1_.jpg',
     imdbScore: 7.5,
     rottenTomatoes: 89,
+    year: 2017,
+    genreTag: ['All', 'Comedy'],
+    whereWatch: ['All', 'Hulu'],
     id: 12
   },
   {
@@ -120,6 +156,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMjE5OTM0OTY5NF5BMl5BanBnXkFtZTgwMDcxOTQ3ODE@._V1_.jpg',
     imdbScore: 7.9,
     rottenTomatoes: 82,
+    year: 2016,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 13
   },
   {
@@ -128,6 +167,9 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BNTUxNzYyMjg2N15BMl5BanBnXkFtZTcwMTExNzExNw@@._V1_.jpg',
     imdbScore: 7.0,
     rottenTomatoes: 91,
+    year: 2011,
+    genreTag: ['All', 'Horror'],
+    whereWatch: ['All', 'Hulu'],
     id: 14
   },
   {
@@ -136,11 +178,68 @@ const dummyData = [
       'https://m.media-amazon.com/images/M/MV5BMTI3MzYxMTA4NF5BMl5BanBnXkFtZTcwMDE4ODg3Mg@@._V1_.jpg',
     imdbScore: 7.4,
     rottenTomatoes: 91,
+    year: 2009,
+    genreTag: ['All'],
+    whereWatch: ['All', 'Hulu'],
     id: 15
+  },
+  {
+    title: 'Booksmart',
+    imgUrl:
+      'https://m.media-amazon.com/images/M/MV5BMjEzMjcxNjA2Nl5BMl5BanBnXkFtZTgwMjAxMDM2NzM@._V1_.jpg',
+    imdbScore: 7.2,
+    rottenTomatoes: 97,
+    year: 2019,
+    genreTag: ['All', 'Comedy'],
+    whereWatch: ['All', 'Hulu'],
+    id: 16
+  },
+  {
+    title: 'The Dark Knight',
+    imgUrl:
+      'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+    imdbScore: 9.0,
+    rottenTomatoes: 94,
+    year: 2008,
+    genreTag: ['All', 'Action/Adventure'],
+    whereWatch: ['All', 'Netflix'],
+    id: 17
   }
 ]
 export class AllMovies extends Component {
+  constructor() {
+    super()
+    this.genreFilter = this.genreFilter.bind(this)
+    this.serviceFilter = this.serviceFilter.bind(this)
+    this.sortByIMDB = this.sortByIMDB.bind(this)
+    this.state = {dummyData: dummyDataOriginal}
+  }
+
+  genreFilter(tag) {
+    let data = this.state.dummyData
+    let filteredData = data.filter(function(movie) {
+      return movie.genreTag.includes(tag)
+    })
+    this.setState({dummyData: filteredData})
+  }
+
+  serviceFilter(tag) {
+    let data = this.state.dummyData
+    let filteredData = data.filter(function(movie) {
+      return movie.whereWatch.includes(tag)
+    })
+    this.setState({dummyData: filteredData})
+  }
+
+  sortByIMDB() {
+    let sortedData = this.state.dummyData.sort(function(movie1, movie2) {
+      return movie2.imdbScore - movie1.imdbScore
+    })
+    this.setState({dummyData: sortedData})
+  }
+
   render() {
+    let dummyData = this.state.dummyData
     return (
       <div className="container">
         <h1 className="flow-text">All Movies Component</h1>
@@ -158,13 +257,16 @@ export class AllMovies extends Component {
             All
           </a>
           <ul id="dropdown4" className="dropdown-content">
-            <li>
+            <li onClick={() => this.genreFilter('All')}>
+              <a href="#!">All</a>
+            </li>
+            <li onClick={() => this.genreFilter('Action/Adventure')}>
               <a href="#!">Action/Adventure</a>
             </li>
-            <li>
+            <li onClick={() => this.genreFilter('Horror')}>
               <a href="#!">Horror</a>
             </li>
-            <li>
+            <li onClick={() => this.genreFilter('Comedy')}>
               <a href="#!">Comedy</a>
             </li>
           </ul>
@@ -200,14 +302,17 @@ export class AllMovies extends Component {
             Hulu
           </a>
           <ul id="dropdown3" className="dropdown-content">
-            <li>
+            <li onClick={() => this.serviceFilter('Netflix')}>
               <a href="#!">Netflix</a>
             </li>
-            <li>
+            <li onClick={() => this.serviceFilter('Hulu')}>
               <a href="#!">Hulu</a>
             </li>
-            <li>
+            <li onClick={() => this.serviceFilter('Amazon')}>
               <a href="#!">Amazon Prime</a>
+            </li>
+            <li onClick={() => this.serviceFilter('All')}>
+              <a href="#!">Every Service</a>
             </li>
           </ul>
         </div>
@@ -227,7 +332,7 @@ export class AllMovies extends Component {
             <li>
               <a href="#!">Alphabetical</a>
             </li>
-            <li>
+            <li onClick={() => this.sortByIMDB()}>
               <a href="#!">IMDB Score</a>
             </li>
             <li>
@@ -241,7 +346,7 @@ export class AllMovies extends Component {
             data-target="dropdown5"
           >
             <i className="material-icons right">arrow_drop_down</i>
-            Filter
+            More Filters
           </a>
           <ul id="dropdown5" className="dropdown-content">
             <li>
@@ -278,6 +383,7 @@ export class AllMovies extends Component {
                         className="responsive-img"
                         alt="something"
                       />
+                      <span className="card-title">{movie.year}</span>
                     </div>
                   </Link>
                   <div className="card-panel hoverable">
