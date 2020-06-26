69e2b04182bcdca45b7da54eb2b53ef8924e0e66
Dylan Horgan <dyhorgan@gmail.com>
2020-06-25 23:50:33 -0400

    test commit

--- client/components/SearchResults.js
+++ client/components/SearchResults.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export const SearchResults = props => {
   return (
     <div>
-      <h1>Did you mean...</h1>
+      <h1>Did you mean: </h1>
       {props.results.map(function(result) {
         return (
           <h2 key={result.id}>
